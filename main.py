387e5a79b6ceeae579b12946c6190033b2830943
雷二猛 <live1989@foxmail.com>
2018-02-25 00:48:10 +0800

    remove listen ip and port

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ middleware.init_app(app)
 blog.register_blueprint(app)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=app.config.get('DEBUG'))
+    app.run(debug=app.config.get('DEBUG'))
